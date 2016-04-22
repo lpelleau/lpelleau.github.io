499b59fcec1992a5784ab3e9590b2909a659f8f4
lpelleau <loic.pelleau@insa-rennes.fr>
2016-04-22 18:33:29 -0500

    Mobile touch

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
-  $('#ct').on('click', function() {
+  function w(e) {
     var m = 'loic';
     m += '@';
     m += 'pelleau.net';
-    $(this).attr('href', 'mailto:' + m);
-  });
+    $(e).attr('href', 'mailto:' + m);
+  };
+  $('#ct').on('click', function() {w(this);});
+  $('#ct').on('tap', function() {w(this);});
 });
