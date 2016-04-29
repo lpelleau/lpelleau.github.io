125caa8d1bbea78bcf71471685b60a8b3e6aaad0
lpelleau <loic.pelleau@insa-rennes.fr>
2016-04-29 12:17:45 -0400

    merge

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
-$(document).ready(function() {
+$(document).ready(function(){
+  $('.parallax').parallax();
+  
   function w(e) {
     var m = 'loic';
     m += '@';
     m += 'pelleau.net';
     $(e).attr('href', 'mailto:' + m);
   };
+  
   $('#ct').on('click', function() {w(this);});
   $('#ct').on('tap', function() {w(this);});
 });
