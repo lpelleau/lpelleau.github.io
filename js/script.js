ae77d46518a047dd6b470c8e3eb501edf328a100
lpelleau <loic@pelleau.net>
2016-07-25 18:51:15 +0100

    remove some shitty code

--- js/script.js
+++ js/script.js
@@ -1,21 +1,3 @@
 $(document).ready(function(){
   $('.parallax').parallax();
-  
-  function w(e) {
-    var m = 'loic';
-    m += '@';
-    m += 'pelleau.net';
-    $(e).attr('href', 'mailto:' + m);
-  };
-  
-  $('#ct').on('click', function() {w(this);});
-  $('#ct').on('tap', function() {w(this);});
-
-  setTimeout(function() {
-    $(".parallax:first img").attr("src", "img/oratoire_st-joseph.jpg");
-    window.scrollTo(0, 500);
-    setTimeout(function() {
-      window.scrollTo(0, 0);
-    }, 10);
-  }, 100);
 });
